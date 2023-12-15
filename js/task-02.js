e79739d67b768c8fa283c04b1a84a4fc16a41e0c
Maxim Noritsyn <maximnoritsyn@gmail.com>
2023-12-15 22:35:07 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const createList = ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  return item;
+}
+);
+
+ingredientsList.append(...createList);
