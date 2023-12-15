a4cc5b0af445dd9f5292492eaf7193f20b03b189
Maxim Noritsyn <maximnoritsyn@gmail.com>
2023-12-15 22:28:57 +0200

    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,8 @@
-const items = document.querySelectorAll('.item');
-items.forEach(element => {
-    
-});
+const items = document.querySelector('#categories');
+console.log(`Number of categories: ${items.children.length}`);
+
+[...items.children].forEach(item => {
+    console.log(`Category: ${item.querySelector('h2').textContent}`);
+    console.log(`Elements: ${item.querySelector('ul').children.length}`);
+}
+);
